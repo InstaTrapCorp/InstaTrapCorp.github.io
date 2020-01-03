25f569facfbe021b5ccabe1f5f6f49608e0ede0c
InstaTrapCorp <58869886+InstaTrapCorp@users.noreply.github.com>
2020-01-03 19:24:29 +0800

    Newly again

--- TestPlugin/src/main/java/io/github/InstaTrapCorp/TestPlugin/TestPlugin.java
+++ TestPlugin/src/main/java/io/github/InstaTrapCorp/TestPlugin/TestPlugin.java
@@ -1,23 +1,12 @@
 package io.github.InstaTrapCorp.TestPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin{
 
 	@Override
 	public void onEnable() {
-		getLogger().info("Plugin has been enabled. Created by instatrapcorp#4225");
-		
-		List<Player> playerList = new ArrayList<Player>();
-		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-		    playerList.add(player);
-		}
-		
+		getLogger().info("Plugin has been enabled. Created by instatrapcorp#4225");		
 		this.getCommand("basic").setExecutor(new TestPluginCommandExecutor(this));
 	}
 	
